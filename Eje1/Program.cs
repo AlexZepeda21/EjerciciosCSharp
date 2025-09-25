1ad691f9a470647ed59ce470441a1b4c74538811
AlexZepeda21 <manuelzepedagr@gmail.com>
2025-09-25 16:14:22 -0600

    Eje1 y Eje2

---
+++ Eje1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Escribir un programa que pida nombre y ead y muestre Hola {nombre} tienes {edad} años.
+
+Console.WriteLine("Hola cual es tu nombre?");
+string nombre = Console.ReadLine();
+Console.WriteLine("Cual es tu edad?");
+string edad = Console.ReadLine();
+
+Console.WriteLine("Hola " + nombre + " tienes " + edad + " años");
---
+++ Eje2/Program.cs
@@ -0,0 +1,68 @@
+﻿//Factorial(iterativo y recursivo).
+
+
+
+//Mi solución de forma iterativa
+
+//Console.WriteLine("Ingrese un numero x");
+//string response = Console.ReadLine();
+
+//int numero = 0;
+
+//numero = Convert.ToInt32(response);
+//int factorial = numero;
+
+//for (int i = 1; i<numero; i++)
+//{
+//    factorial *= i;
+//}
+
+//Console.WriteLine("El factorial del numero es " + factorial);
+
+
+
+//Solución mas optima de forma iterativa
+
+//Console.WriteLine("Ingrese un numero x");
+//string r = Console.ReadLine();
+
+//int n = Convert.ToInt32(r);
+//int factorial = 1;
+
+//for (int i = 2; i <= n; i++)
+//{
+//    factorial *= i;
+//}
+
+//Console.WriteLine("El factorial de " + n + " es " + factorial);
+
+
+//Solución de forma recursiva
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un numero");
+        string r = Console.ReadLine();
+
+        int n = Convert.ToInt32(r);
+
+        int resultado = Factorial(n);
+
+        Console.WriteLine(resultado);
+       
+    }
+    static int Factorial(int n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Factorial(n - 1);
+        }
+    }
+
+}
