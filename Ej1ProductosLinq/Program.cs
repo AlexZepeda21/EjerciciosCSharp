eada05299896ca39b35567e1b8d8a79d5d1a2a2d
AlexZepeda21 <manuelzepedagr@gmail.com>
2025-10-05 22:43:03 -0600

    EJ1ProductosLinq : Primer ejercicio donde se implementa Linq

---
+++ Ej1ProductosLinq/Program.cs
@@ -0,0 +1,29 @@
+﻿using Ej1ProductosLinq;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<Producto> products = new List<Producto>
+        {
+            new Producto{Nombre = "Manzana", Categoria= "fruta", Precio = 10.0},
+            new Producto{Nombre = "Platano", Categoria = "fruta", Precio = 10.0},
+            new Producto{Nombre = "Rabano", Categoria = "verdura", Precio = 5.0},
+            new Producto{Nombre = "Berro", Categoria = "verdura", Precio = 5.0}
+
+        };
+        var agrupados = products
+           .GroupBy(p => p.Categoria)
+           .Select(g => new
+           {
+               Categoria = g.Key,
+               Subtotal = g.Sum(p => p.Precio)
+           });
+
+        // Mostrar resultados
+        foreach (var grupo in agrupados)
+        {
+            Console.WriteLine($"{grupo.Categoria}: {grupo.Subtotal}");
+        }
+    }
+}
---
+++ Ej2BubbleSectionInsertionSort/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = 11;
+        int[] numeroArray = { 0 };
+        while (numero == 11)
+        {
+            Console.WriteLine("Ingrese un numero");
+            int n = Convert.ToInt32(Console.ReadLine());
+            numeroArray.Append<n>;
+        }
+    }
+    
+}
